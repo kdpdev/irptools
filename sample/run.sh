ead82a8a3c90514686e16fe0ca1d21efa6f8544a
kdpdev <kdp.dev@gmail.com>
2024-02-25 00:55:49 +0300

    fix run.sh

--- sample/run.sh
+++ sample/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-irptools.exe -cmd=parse -cfg=cfg_parse.json
-irptools.exe -cmd=filter -cfg=cfg_filter.json
+./irptools.exe -cmd=parse -cfg=cfg_parse.json
+./irptools.exe -cmd=filter -cfg=cfg_filter.json
